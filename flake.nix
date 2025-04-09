00a6bc0381b702765f5df55267dad970fe3de705
Niklas Thörne <niklas.thorne@netclean.com>
2025-04-09 10:06:56 +0200

    Added nix devShell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "OFC dev shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          clang
+          clang-tools
+          conan
+          cmake
+          ninja
+          pkg-config
+        ];
+      };
+    });
+}
